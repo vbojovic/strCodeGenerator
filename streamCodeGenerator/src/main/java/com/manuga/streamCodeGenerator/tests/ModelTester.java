928f4f253cfa75aace54a7a3e036b6621068b2c7
Viktor Bojovic <viktor.bojovic@gmail.com>
2018-04-09 17:20:13 +0200

    test remarked..

--- streamCodeGenerator/src/main/java/com/manuga/streamCodeGenerator/tests/ModelTester.java
+++ streamCodeGenerator/src/main/java/com/manuga/streamCodeGenerator/tests/ModelTester.java
@@ -48,7 +48,7 @@ public class ModelTester {
 		DatabaseModel dbm = new DatabaseModel();
 		dbm.loadFromXml(importFileName);
 	}
-	@Test
+//	@Test
 	public void testMysqlInnerModelExportAll() throws Exception {
 		MysqDb db = new MysqDb();
 		//db.setSettings(Helper.homeSettings());
@@ -67,7 +67,7 @@ public class ModelTester {
 	}
 
 
-	@Test
+//	@Test
 	public void testInnerModelExportAll() throws Exception {
 		PgDb db = new PgDb();
 		//db.setSettings(Helper.homeSettings());
